a6ff2641678e8cc064b9ef0e9da775db0696cf75
ariola <ariola.markou@gmail.com>
2023-06-16 16:28:19 +0300

    Solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,74 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength;
+    }
+    receiveDamage(damaged) {
+        this.health -= damaged;
+    }
+}
 
 // Viking
-class Viking {}
-
-// Saxon
-class Saxon {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        } else {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+}
+//
+class Saxon extends Soldier {
+    receiveDamage(damage) {
+        super.receiveDamage(damage);
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`;
+        } else {
+            return `A Saxon has died in combat`;
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(vikingArmy = [], saxonArmy = []) {
+        this.vikingArmy = vikingArmy;
+        this.saxonArmy = saxonArmy;
+    }
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    }
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    }
+    vikingAttack() {
+        let randomSaxonNumber = Math.floor(
+            Math.random() * this.saxonArmy.length
+        );
+        let randomSaxon = this.saxonArmy[randomSaxonNumber];
+        let randomVikingNumber = Math.floor(
+            Math.random() * this.vikingArmy.length
+        );
+        let randomViking = this.vikingArmy[randomVikingNumber];
+        let result = randomSaxon.receiveDamage(randomViking.strength);
+        if (randomSaxon.health <= 0) {
+            this.saxonArmy.splice(randomSaxonNumber, 1);
+        }
+        return result;
+    }
+    saxonAttack() {}
+    showStatus() {}
+}
